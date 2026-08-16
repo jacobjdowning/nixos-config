dfd8ba2ac5744af5be78cfb997a19a32dad3add4
Jake Downing <jacobjdowning@gmail.com>
2026-08-16 14:16:55 -0400

    Installed FiraCode font

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,8 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  fonts.packages = with pkgs; [ nerd-fonts.fira-code ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
