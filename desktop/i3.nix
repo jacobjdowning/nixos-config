cbd34dff820d4c2fa047604ad1b1f415911f76f1
Jake Downing <jacobjdowning@gmail.com>
2026-09-09 23:51:20 -0400

    gaps added

--- desktop/i3.nix
+++ desktop/i3.nix
@@ -1,10 +1,36 @@
 { lib, ...}:
 {
-   xsession.windowManager.i3.enable = true;
-   xsession.windowManager.i3.config.keybindings = lib.mkOptionDefault {
-      "mod1+b" = "exec firefox";
-      "mod1+p" = "exec rofi -show drun";
-   };
-   xsession.windowManager.i3.config.terminal = "alacritty";
-   xsession.windowManager.i3.config.bars = [];
+	xsession.windowManager.i3.enable = true;
+	xsession.windowManager.i3.config = {
+		keybindings = lib.mkOptionDefault {
+      			"mod1+b" = "exec firefox";
+      			"mod1+p" = "exec rofi -show drun";
+   		};
+   		terminal = "alacritty";
+   		bars = [];
+		gaps = {
+			inner = 6;
+			outer = 4;
+		};
+		window = {
+			titlebar = false;
+		};
+		colors = {
+			focused = {
+				border = "#4F422E";
+				background = "#4F422E";
+				indicator = "#4F422E";
+				childBorder = "#4F422E";
+				text = "#282828";
+			};
+			unfocused = {
+				border = "#282828";
+				background = "#282828";
+				indicator = "#282828";
+				childBorder = "#282828";
+				text = "#d4BE98";
+			};
+				
+		};
+	};
 }
