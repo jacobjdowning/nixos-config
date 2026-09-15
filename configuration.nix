85a9d54a6f16357c2164217ef8eb43f8af72c3fa
Jake Downing <jacobjdowning@gmail.com>
2026-09-15 02:10:56 -0400

    Added printer discovery and drivers

--- configuration.nix
+++ configuration.nix
@@ -66,6 +66,18 @@
 
   services.udev.packages = [ (pkgs.callPackage ./g13/pkg.nix { }) ];
 
+  #Printing
+  services.printing = {
+  	enable = true;
+	drivers = [ pkgs.gutenprint ];
+  };
+
+  services.avahi = {
+  	enable = true;
+	nssmdns4 = true;
+	openFirewall = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
